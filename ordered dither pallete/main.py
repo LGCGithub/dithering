fbf624f89273f419071f8de2d2f1142031d6af49
Roykaah <74203689+Roykaah@users.noreply.github.com>
2022-06-08 19:10:46 -0300

    Add files via upload

--- ordered dither pallete/main.py
+++ ordered dither pallete/main.py
@@ -6,10 +6,10 @@ directory_output = "output/"
 
 directory = os.listdir(directory_input)
 
-length = 4 # Valores: 2, 4, 8
+length = 1 # Valores: 2, 4, 8
 size = 16 # Tamanho da paleta
-mode = 3 # 1 == EGA pallete, 2 == random pallete
-interacoes = 100
+mode = 3 # 1 == EGA pallete, 2 == random pallete, 3 == k-means pallete
+interacoes = 1
 
 for filename in directory:
     command = "./main " + directory_input + filename + " " + directory_output + filename + " " + str(length) + " " + str(mode) + " " + str(size) + " " + str(interacoes)
